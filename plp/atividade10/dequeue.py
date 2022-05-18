2a7f8b22c7916af8db6b290200cb9d212fc7e91a
alan <alann678n@gmail.com>
2022-05-18 19:50:23 -0300

    ativ 11 plp done

--- plp/atividade10/dequeue.py
+++ plp/atividade10/dequeue.py
@@ -1,43 +1,43 @@
 import gc
 
+
 class Node:
-    def __init__(self,element):
+    def __init__(self, element):
         self.__element = element
         self.__next = None
         self.__previous = None
-    
+
     def __del__(self):
         del self
         gc.collect()
-        return "Node eliminated" 
-    
+        return "Node eliminated"
+
     def getElement(self):
         return self.__element
-    
+
     def getPrevious(self):
         return self.__previous
-    
+
     def setPrevious(self, previous):
         self.__previous = previous
-    
+
     def getNext(self):
         return self.__next
-    
-    def setNext(self,next):
+
+    def setNext(self, next):
         self.__next = next
-    
+
 
 class Dequeue:
-    def __init__(self,max: int):
+    def __init__(self, max: int):
         self.__max = max
         self.__header: Node = None
         self.__tailer: Node = None
 
-
     def __del__(self):
         del self
         gc.collect()
-        return "Dequeue eliminated" 
+        return "Dequeue eliminated"
 
     def empty(self):
         if not self.__header and not self.__tailer:
@@ -46,14 +46,14 @@ class Dequeue:
             return False
 
     def size(self):
-            size = 0
-            element = self.__header
-            while element is not None:
-                size+=1
-                element = element.getNext()
-            return size
-    
-    def offerFirst(self,element):
+        size = 0
+        element = self.__header
+        while element is not None:
+            size += 1
+            element = element.getNext()
+        return size
+
+    def offerFirst(self, element):
         if self.size() < self.__max:
             NewNode = Node(element)
             if self.__header is not None:
@@ -63,15 +63,13 @@ class Dequeue:
                 self.__header.setNext(aux)
             else:
                 self.__header = NewNode
-                self.__tailer =  self.__header
+                self.__tailer = self.__header
             return True
-        
-
 
     def offerLast(self, element):
         if self.__max > self.size():
             NewNode = Node(element)
-            if self.__tailer is None and self.__header is None :
+            if self.__tailer is None and self.__header is None:
                 self.__header = NewNode
                 self.__tailer = self.__header
             else:
@@ -81,20 +79,18 @@ class Dequeue:
                 self.__tailer.setPrevious(last)
             return True
 
-
     def peekFirst(self):
-        if  not self.empty():
+        if not self.empty():
             return self.__header.getElement()
         else:
             return None
-    
-    
+
     def peekLast(self):
         if not self.empty():
             return self.__tailer.getElement()
         else:
             return None
-    
+
     def pollFirst(self):
         mostrar = self.__header
         self.__header = self.__header.getNext()
@@ -105,4 +101,3 @@ class Dequeue:
         self.__tailer = self.__tailer.getPrevious()
         self.__tailer.setNext(None)
         return mostrar.getElement()
-    
--- plp/atividade11/threads_primos.py
+++ plp/atividade11/threads_primos.py
@@ -1,9 +1,51 @@
-# Desenvolva um programa que apresente os números primos existentes entre 0 e 99999.
-# Para cada faixa de dez mil valores, crie uma Thread e dispare o processo para a faixa de valores correspondente.
-#
-# Observações:
-# 1) A implementação do exercício pode ser realizada na linguagem de programação de preferência do usuário,
-# desde que a mesma seja uma linguagem de programação com suporte a concorrência;
-# 2) Postar como solução do exercício um arquivo compactado, contendo todos os arquivos necessários para a execução do mesmo,
-# inclusive, um arquivo denominado README.txt com as instruções de como compilar e executar a solução desenvolvida.
+# Aluno: Alan da Mota Nascimento
+# Mátricula: 2012130037
+import math
+import threading
 
+
+def isprimo(numero: int) -> bool:
+
+    if numero == 0 or numero == 1:
+        return False
+    if numero == 2:
+        return True
+    if numero > 2 and numero % 2 == 0:
+        return False
+    max_divisor = math.floor(math.sqrt(numero))
+    for x in range(3, 1 + max_divisor, 2):
+        if numero % x == 0:
+            return False
+    return True
+
+
+def mostraprimos(range_inicial: int, range_final: int):
+    lista_primos = []
+    for i in range(range_inicial, range_final):
+        if isprimo(i):
+            lista_primos.append(i)
+    print(f'lista dos primos achados pela thread responsavel pelo intervalo {range_inicial} - {range_final}:\n{lista_primos}')
+
+def main():
+    thread1 = threading.Thread(target=mostraprimos, args=(0, 10000))
+    thread2 = threading.Thread(target=mostraprimos, args=(10000, 20000))
+    thread3 = threading.Thread(target=mostraprimos, args=(20000, 30000))
+    thread4 = threading.Thread(target=mostraprimos, args=(30000, 40000))
+    thread5 = threading.Thread(target=mostraprimos, args=(50000, 60000))
+    thread6 = threading.Thread(target=mostraprimos, args=(60000, 70000))
+    thread7 = threading.Thread(target=mostraprimos, args=(70000, 80000))
+    thread8 = threading.Thread(target=mostraprimos, args=(80000, 90000))
+    thread9 = threading.Thread(target=mostraprimos, args=(90000, 99999))
+
+    thread1.run()
+    thread2.run()
+    thread3.run()
+    thread4.run()
+    thread5.run()
+    thread6.run()
+    thread7.run()
+    thread8.run()
+    thread9.run()
+
+if __name__ == "__main__":
+    main()
