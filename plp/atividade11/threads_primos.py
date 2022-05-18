7d5d44739b349ba45a06450cc237f5be753cba6d
alan <alann678n@gmail.com>
2022-05-18 19:46:32 -0300

    ativ 11 plp

---
+++ plp/atividade11/threads_primos.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que apresente os números primos existentes entre 0 e 99999.
+# Para cada faixa de dez mil valores, crie uma Thread e dispare o processo para a faixa de valores correspondente.
+#
+# Observações:
+# 1) A implementação do exercício pode ser realizada na linguagem de programação de preferência do usuário,
+# desde que a mesma seja uma linguagem de programação com suporte a concorrência;
+# 2) Postar como solução do exercício um arquivo compactado, contendo todos os arquivos necessários para a execução do mesmo,
+# inclusive, um arquivo denominado README.txt com as instruções de como compilar e executar a solução desenvolvida.
+
